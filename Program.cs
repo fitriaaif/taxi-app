f112dde421306b8d74ffa0b83ac66475f5ad2d32
Fitria Amanda <101631102+fitriaaif@users.noreply.github.com>
2022-03-23 21:40:10 +0700

    program taxi

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxiApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Taxi taxi = new Taxi();
+
+            taxi.DriverName = "Mas Yusup";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 7;
+
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
